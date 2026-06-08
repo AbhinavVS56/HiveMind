276ad949eec59112ffe84d069a531c129f3e53a1
Abhinav <abhinavsuresh61058@gmail.com>
2026-06-09 01:07:43 +0530

    HiveMind prototype2/V2 with decision node which uses llm

--- backend/researcher_agent.py
+++ backend/researcher_agent.py
@@ -1,6 +1,7 @@
 from typing import TypedDict
 from langchain_ollama import ChatOllama
 from langgraph.graph import StateGraph, START,END
+#import time
 
 class ResearchState(TypedDict):
     query:str
@@ -8,6 +9,7 @@ class ResearchState(TypedDict):
     analysis:str
     critique:str
     final_answer:str
+    need_critic:bool
 
 llm=ChatOllama(
     model="qwen3:8b",
@@ -15,32 +17,74 @@ llm=ChatOllama(
 )
 
 def research_agent(state: ResearchState):
+    #start=time.time()
     prompt=f"""
     You are an expert researcher.
-    Research the following topic and provide concise factual information.
-    Maximum 5 bullet points.
+    Research the following topic.
+    Provide:
+    - Maximum 3 bullet points
+    - Maximum 80 words total
+    - Only factual information
     Topic:
     {state["query"]}
     """
     response=llm.invoke(prompt)
+    #print(f"Researcher took {time.time()-start:.2f} seconds")
     return{
         "research":response.content
     }
 
 def analysis_agent(state: ResearchState):
+    #start=time.time()
     prompt=f"""
     You are an expert analyst.
-    Analyze the research below and provide 3 key insights only.
-    Maximum 150 words.
+    Analyze the research below and provide exactly 2 key insights.
+    Maximum 80 words.
     Research:
     {state['research']}
     """
     response=llm.invoke(prompt)
+    #print(f"Analyst took {time.time()-start:.2f} seconds")
     return{
         "analysis":response.content
     }
 
+def decision_agent(state: ResearchState):
+    #start=time.time()
+    prompt=f"""
+    You are a workflow manager.
+    A critique step is required when:
+    -The query asks for comparisions
+    -The query involves opinions
+    -The query involves trade-offs
+    -The query involves recommendations
+    -The query involves controversial topics
+
+    A critique step is NOT needed when:
+    -The query asks for simple facts
+    -The query asks for definitions
+    -The query asks for straightforward explanations
+
+    Return only YES or NO
+    Query:
+    {state['query']}
+    """
+    response=llm.invoke(prompt)
+    answer=response.content.upper()
+    #print("RAW RESPONSE:")
+    #print(response.content)
+    #print(f"Decision Agent took {time.time()-start:.2f} seconds")
+    return{
+        "need_critic": "YES" in answer
+    }
+
+def route_decision(state: ResearchState):
+    if state["need_critic"]:
+        return "critic"
+    return "writer"
+
 def critic_agent(state: ResearchState):
+    #start=time.time()
     prompt=f"""
     You are an expert critic.
     Review the research and analysis below.
@@ -49,24 +93,26 @@ def critic_agent(state: ResearchState):
     -Missing information
     -Possible inaccuracies
     -Alternative viewpoints
-    Provide at most 3 criticisms.
-    Maximum 150 words.
+    Provide at most 2 criticisms.
+    Maximum 80 words.
     Research:
     {state['research']}
     Analysis:
     {state['analysis']}
     """
     response=llm.invoke(prompt)
+    #print(f"Critic took {time.time()-start:.2f} seconds")
     return{
         "critique":response.content
     }
 
 def writer_agent(state: ResearchState):
+    #start=time.time()
     prompt=f"""
     You are an expert technical writer.
-    Using the research, analysis and crtique below,
+    Using the research, analysis and critique below,
     create a clear, balanced and well structured final answer.
-    Maximum 300 words.
+    Maximum 150 words.
     Research:
     {state['research']}
     Analysis:
@@ -75,6 +121,7 @@ def writer_agent(state: ResearchState):
     {state['critique']}
     """
     response=llm.invoke(prompt)
+    #print(f"Writer took {time.time()-start:.2f} seconds")
     return{
         "final_answer":response.content
     }
@@ -85,22 +132,32 @@ graph_builder.add_node("researcher",research_agent)
 graph_builder.add_node("analyst",analysis_agent)
 graph_builder.add_node("critic",critic_agent)
 graph_builder.add_node("writer",writer_agent)
+graph_builder.add_node("decision",decision_agent)
 
 graph_builder.add_edge(START,"researcher")
 graph_builder.add_edge("researcher","analyst")
-graph_builder.add_edge("analyst","critic")
+graph_builder.add_edge("analyst","decision")
+graph_builder.add_conditional_edges("decision",
+                                    route_decision,
+                                    {
+                                        "critic":"critic",
+                                        "writer":"writer"
+                                    }
+                                )
 graph_builder.add_edge("critic","writer")
 graph_builder.add_edge("writer",END)
 
 graph=graph_builder.compile()
+#print(graph.get_graph().draw_mermaid())
 
 test_state={
-    "query":"What is LangGraph",
+    "query":"Compare LangGraph and CrewAI",
     "research":"",
     "analysis":"",
+    "need_critic":"",
     "critique":"",
     "final_answer":""
 }
 
 result=graph.invoke(test_state)
-print(result)
+print(result["final_answer"])
