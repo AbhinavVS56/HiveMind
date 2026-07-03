a783325486a8a1edac97928f1601504e5d0a5626
Abhinav <abhinavsuresh61058@gmail.com>
2026-07-04 00:55:30 +0530

    UI changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react"
 import "./App.css"
+import logo from "./assets/logo.png"
+import background from "./assets/background.jpg"
+import mindflayer from "./assets/mindflayer.jpg"
 
 function App(){
   const[query,setQuery]=useState("")
@@ -25,27 +28,34 @@ function App(){
   }
 
   return(
-    <div className="app">
-      <h1 className="title">HiveMind</h1>
-      <textarea className="query-input"
-              placeholder="Ask anything" 
-              value={query} 
-              onChange={(event)=>setQuery(event.target.value)}>
-      </textarea>
-      <button className="ask-button"
-              onClick={sendQuestion} 
-              disabled={loading}>{loading?"Thinking...":"Ask HiveMind"}</button>
-      <h2 className="convo-title">Conversation</h2>
-      <div className="convo">  
-        {messages.map((message,index)=>(
-        <div key={index} className={`message ${message.sender === "You" ? "user" : "hivemind"}`}>
-          <h3 className="sender">{message.sender}</h3>
-          <p className="content">{message.text}</p>
+    messages.length===0?(
+    <div className="landing-page">
+      <img src={logo} alt="HiveMind Logo" className="logo"/>
+      <div className="input-section">
+        <textarea className="query-input"
+                placeholder="What's on your mind?" 
+                value={query} 
+                onChange={(event)=>setQuery(event.target.value)}>
+        </textarea>
+        <div className="button">
+          <button className="ask-button"
+                  onClick={sendQuestion} 
+                  disabled={loading}>{loading?"Thinking...":"Ask HiveMind"}</button>
         </div>
-      ))
-      }
       </div>
+      <p className="quote">
+        <em>"I am on a curiosity voyage, and I need my paddles to travel.
+        These books... these books are my paddles."
+        </em>
+        <br/>
+        <span>- Dustin Henderson</span>
+      </p>
     </div>
+    ):(
+    <div className="chat-page">
+        Chat Page
+    </div>
+    )
   )
 }
 
